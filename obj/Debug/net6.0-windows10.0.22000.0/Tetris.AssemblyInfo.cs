e29fc3412a6c317248d9636a2284eb26d54e003b
miojorc <91229107+miojorc@users.noreply.github.com>
2024-09-24 21:18:51 -0300

    Tetris 1.1.44
    AutoGenerateCodes

--- obj/Debug/net6.0-windows10.0.22000.0/Tetris.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows10.0.22000.0/Tetris.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bd5cf65e1ea5953fd1ec02e013618583270add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5fa0b505df67c12422577cf8786a43694c2103")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
